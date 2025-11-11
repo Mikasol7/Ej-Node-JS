05574aef8aff190d1a3445148e3f554699ba3e88
Mikasol7 <micaelasol-l@hotmail.com>
2025-11-11 19:54:04 +0000

    Clase 10 nov

--- src/login.js
+++ src/login.js
@@ -3,18 +3,33 @@ const username = document.getElementById("username")
 const password = document.getElementById("password")
 const boton = document.getElementById("obtener")
 
+const ATO = localStorage.getItem("ATO")
+
+if (ATO) {
+    window.location.href = '/'
+
+}
+
 boton.onclick = () => {
-    const body = JSON.stringify({ username: username.value, password: password.value })
-    fetch(`${URL}/auth`, {
-        method: 'POST',
+    const body = JSON.stringify({ username: username.value, password: password.value }) 
+    fetch(`${URL}/auth`, { 
+        method: 'POST', 
         body,
         headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json" 
         }
     })
-        .then((response) => response.json())
+        .then((response) => response.json()) 
         .then((json) => {
             console.log(json)
-            localStorage.setItem ("ATO", json.ATO)
-        });
+            if (json.ATO) {
+                localStorage.setItem("ATO", json.ATO) 
+            }
+
+        }).catch((err) => {  
+            const { message } = err 
+            console.log(message)
+        }
+        )
 }
+
--- src/main.js
+++ src/main.js
@@ -1,31 +1,47 @@
-const URL = import.meta.env.VITE_API_URL
-
+const URL = import.meta.env.VITE_API_URL 
 const obtener = document.getElementById("obtener")
 const tareas = document.getElementById("tareas")
+const ATO = localStorage.getItem("ATO")
 
 function crearTarjeta(title, completed) {
   const div = document.createElement("div")
   div.classList.add("card")
-  div.innerHTML = `<span>${title}</span> <span>${completed ? "SI" : "NO"}</span>`
+  div.innerHTML = `<span>${title}</span> <span class="completado">${completed ? "SI" : "NO"}</span>`
+  div.getElementsByClassName("completado")[0]
+  const completado = div.getElementsByClassName("completado")[0]
+  completado.onclick = () => {
+    console.log(holapapu)
+  }
   return div
 }
 
-function obtenerTareasPaginadas (page=0,take=5) {
-  fetch(`${URL}/tasks?page=${page}&take=${take}`)
-    .then((response) => response.json())
-    .then((json) => {
+function obtenerTareasPaginadas(page = 0, take = 5) {
+  fetch(`${URL}/tasks?page=${page}&take=${take}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": ATO 
+    }
+  })
+    .then((a) => a.json()) 
+    .then(json => {
+      console.log(json)
       json.data.forEach(element => {
         const card = crearTarjeta(element.title, element.completed)
         tareas.appendChild(card)
       });
-    const cantPages= Math.ceil(json.total_result/take)
+      const cantPages = Math.ceil(json.total_result / take)
     });
 }
 
-function crearComponentePaginado (cantPages, page) {
-  for (let i=0; i<cantPages; i++){
-    crearboton (i)
+function crearComponentePaginado(cantPages, page) {
+  for (let i = 0; i < cantPages; i++) {
+    crearboton(i)
   }
 }
 
-obtenerTareasPaginadas()
+if (!ATO) { 
+  window.location.href = "login"
+} else {
+  obtenerTareasPaginadas()
+}
